75f1c47c7240bca80e46276bff90bc9e0eda0c6d
Michael Hutchinson <mick.hutchinson@outlook.comgit inittouch test.txtgit add .git commit -m First>
2019-10-27 14:31:35 +0000

    Added toast method to prompt user to enter a hillfort is button is pressed without entry

--- app/src/main/java/org/wit/hillfort/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/HillfortActivity.kt
@@ -2,8 +2,10 @@ package org.wit.hillfort
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {  //Includes AnkoLogger feature
 
@@ -11,5 +13,17 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {  //Includes AnkoLogge
         info("Hillfort Activity started")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+
+        //Event handler for button
+        btnAdd.setOnClickListener() {
+            val hillfortTitle = hillfortTitle.text.toString()
+            if (hillfortTitle.isNotEmpty()) {
+                info("add Button Pressed: $hillfortTitle")
+            }
+            else {
+                //Will ask for entry if left blank
+                toast ("Please Enter a title")
+            }
+        }
     }
 }
