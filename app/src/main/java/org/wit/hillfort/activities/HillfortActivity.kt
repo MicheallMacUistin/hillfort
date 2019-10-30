113b4722df9b3c5683dbbb04ee0abe9126cbd570
Michael Hutchinson <mick.hutchinson@outlook.comgit inittouch test.txtgit add .git commit -m First>
2019-10-30 09:46:21 +0000

    Created new Main App Class in new Main package. Still with errors...

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -7,28 +7,30 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {  //Includes AnkoLogger feature
 
     //Creating a hillfort as a class member
     var hillfort = HillfortModel()
-    //Creating an arraylist of hillforts
-    var hillforts = ArrayList<HillfortModel>()
+    //Refer to MainApp object with null safety '?'
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
+        app = application as MainApp
 
         //Event handler for button
         btnAdd.setOnClickListener() {
             hillfort.title = hillfortTitle.text.toString()
             hillfort.description = description.text.toString()
             if (hillfort.title.isNotEmpty()) {
-                hillforts.add(hillfort.copy())
-                info("add Button Pressed: $hillfort")
-                for(i in hillforts.indices){
-                    info("Hillfort[$i]:${this.hillforts[i]}")
+                app.hillforts.add(hillfort.copy())
+                info("add Button Pressed: ${hillfort}")
+                for(i in app.hillforts.indices){
+                    info("Hillfort[$i]:${app.hillforts[i]}")
                 }
             }
             else {
