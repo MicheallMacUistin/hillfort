64ad37836e45a968db16c33570b0002ba1fc0845
Michael Hutchinson <mick.hutchinson@outlook.comgit inittouch test.txtgit add .git commit -m First>
2019-11-18 17:11:02 +0000

    Refactored project including introduction of new classes

---
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -0,0 +1,75 @@
+package org.wit.hillfort.views.hillfort
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.helpers.readImageFromPath
+import org.wit.hillfort.models.HillfortModel
+
+class HillfortView : AppCompatActivity(), AnkoLogger {
+
+    lateinit var presenter: HillfortPresenter
+    var hillfort = HillfortModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+
+        presenter = HillfortPresenter(this)
+
+        btnAdd.setOnClickListener {
+            if (hillfortTitle.text.toString().isEmpty()) {
+                toast(R.string.enter_hillfort_title)
+            } else {
+                presenter.doAddOrSave(hillfortTitle.text.toString(), description.text.toString())
+            }
+        }
+
+        chooseImage.setOnClickListener { presenter.doSelectImage() }
+
+        hillfortLocation.setOnClickListener { presenter.doSetLocation() }
+    }
+
+    fun showHillfort(hillfort: HillfortModel) {
+        hillfortTitle.setText(hillfort.title)
+        description.setText(hillfort.description)
+        hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
+        if (hillfort.image != null) {
+            chooseImage.setText(R.string.change_hillfort_image)
+        }
+        btnAdd.setText(R.string.save_hillfort)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        if (presenter.edit) menu.getItem(0).setVisible(true)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_delete -> {
+                presenter.doDelete()
+            }
+            R.id.item_cancel -> {
+                presenter.doCancel()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            presenter.doActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
--- app/src/main/java/org/wit/hillfort/views/hillfortList/HillfortListView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfortList/HillfortListView.kt
@@ -1,5 +1,4 @@
-package org.wit.hillfort.activities
-
+package org.wit.hillfort.views.hillfortList
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,38 +6,27 @@ import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.startActivityForResult
 import org.wit.hillfort.R
-import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
+import org.wit.hillfort.views.hillfortList.HillfortAdapter
+import org.wit.hillfort.views.hillfortList.HillfortListPresenter
+import org.wit.hillfort.views.hillfortList.HillfortListener
 
+class HillfortListView : AppCompatActivity(), HillfortListener {
 
-class HillfortListActivity : AppCompatActivity(), HillfortListener {
-
-    lateinit var app: MainApp
+    lateinit var presenter: HillfortListPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort_list)
-        app = application as MainApp
-
         toolbar.title = title
         setSupportActionBar(toolbar)
 
+        presenter = HillfortListPresenter(this)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = HillfortAdapter(app.hillforts.findAll(), this)
-        loadHillforts()
-    }
-
-    private fun loadHillforts() {
-        showHillforts(app.hillforts.findAll())
-    }
-
-    fun showHillforts (hillforts: List<HillfortModel>) {
-        recyclerView.adapter = HillfortAdapter(hillforts, this)
+        recyclerView.adapter =
+            HillfortAdapter(presenter.getHillforts(), this)
         recyclerView.adapter?.notifyDataSetChanged()
     }
 
@@ -49,20 +37,18 @@ class HillfortListActivity : AppCompatActivity(), HillfortListener {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
-            R.id.item_add -> startActivityForResult<HillfortActivity>(200)
-            R.id.item_map -> startActivity<HillfortMapsActivity>()
+            R.id.item_add -> presenter.doAddHillfort()
+            R.id.item_map -> presenter.doShowHillfortsMap()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    //Passing selected hillfort to activity
     override fun onHillfortClick(hillfort: HillfortModel) {
-        startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
+        presenter.doEditHillfort(hillfort)
     }
 
-    //Refresh the view after editing
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        loadHillforts()
+        recyclerView.adapter?.notifyDataSetChanged()
         super.onActivityResult(requestCode, resultCode, data)
     }
 }
