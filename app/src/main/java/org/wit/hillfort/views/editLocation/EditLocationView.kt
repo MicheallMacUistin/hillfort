5e426777f83d49a4ad8010e003061b3843bdf409
Michael Hutchinson <mick.hutchinson@outlook.comgit inittouch test.txtgit add .git commit -m First>
2019-11-25 12:11:56 +0000

    Converted EditLocation & Presenter to use base classes

--- app/src/main/java/org/wit/hillfort/views/editLocation/EditLocationView.kt
+++ app/src/main/java/org/wit/hillfort/views/editLocation/EditLocationView.kt
@@ -1,14 +1,13 @@
-package org.wit.hillfort.views.editLocation
+package org.wit.hillfort.views.editlocation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.Marker
 import org.wit.hillfort.R
-import org.wit.hillfort.views.editLocation.EditLocationPresenter
+import org.wit.hillfort.views.BaseView
 
-class EditLocationView : AppCompatActivity(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
+class EditLocationView : BaseView(), GoogleMap.OnMarkerDragListener, GoogleMap.OnMarkerClickListener {
 
     lateinit var map: GoogleMap
     lateinit var presenter: EditLocationPresenter
@@ -22,7 +21,7 @@ class EditLocationView : AppCompatActivity(), GoogleMap.OnMarkerDragListener, Go
             map = it
             map.setOnMarkerDragListener(this)
             map.setOnMarkerClickListener(this)
-            presenter.initMap(map)
+            presenter.doConfigureMap(map)
         }
     }
 
@@ -31,11 +30,11 @@ class EditLocationView : AppCompatActivity(), GoogleMap.OnMarkerDragListener, Go
     override fun onMarkerDrag(marker: Marker) {}
 
     override fun onMarkerDragEnd(marker: Marker) {
-        presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude, map.cameraPosition.zoom)
+        presenter.doUpdateLocation(marker.position.latitude, marker.position.longitude)
     }
 
     override fun onBackPressed() {
-        presenter.doOnBackPressed()
+        presenter.doSave()
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
